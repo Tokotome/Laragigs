edac48a74db0a295caacd36dd7d8fb42a91e2577
Tokotome <mandzhukov.bg@gmail.com>
2023-03-04 11:43:02 +0200

    add delete functionality

--- routes/web.php
+++ routes/web.php
@@ -35,8 +35,11 @@ Route::post('/listings', [ListingController::class, 'store']);
 //Show edit form
 Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
 
-// Update listing
+// Update listing 
 Route::put('/listings/{listing}', [ListingController::class, 'update']);
+
+// Delete listing 
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 
 //Single listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
